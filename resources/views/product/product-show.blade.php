7bb69c5843d222f964acdd309f2895430a7b9f78
David124272 <david.maciasa@alumnos.udg.mx>
2021-05-27 00:33:34 -0500

    js added to public

--- resources/views/product/product-show.blade.php
+++ resources/views/product/product-show.blade.php
@@ -1,4 +1,71 @@
 @extends('layouts/navbar')
 @section('content')
     <h1>Ver producto</h1>
+
+    <!-- slider Area Start-->
+    <div class="slider-area ">
+        <!-- Mobile Menu -->
+        <div class="single-slider slider-height2 d-flex align-items-center" data-background="assets/img/hero/category.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hero-cap text-center">
+                            <h2>product Details</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider Area End-->
+
+    <!--================Single Product Area =================-->
+    <div class="product_image_area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="product_img_slide owl-carousel">
+                        <div class="single_product_img">
+                            <img src="assets/img/product/single_product.png" alt="#" class="img-fluid">
+                        </div>
+                        <div class="single_product_img">
+                            <img src="assets/img/product/single_product.png" alt="#" class="img-fluid">
+                        </div>
+                        <div class="single_product_img">
+                            <img src="assets/img/product/single_product.png" alt="#" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="single_product_text text-center">
+                        <h3>Foam filling cotton slow <br>
+                            rebound pillows</h3>
+                        <p>
+                            Seamlessly empower fully researched growth strategies and interoperable internal or
+                            “organic” sources. Credibly innovate granular internal or “organic” sources whereas high
+                            standards in web-readiness. Credibly innovate granular internal or organic sources whereas
+                            high standards in web-readiness. Energistically scale future-proof core competencies
+                            vis-a-vis impactful experiences. Dramatically synthesize integrated schemas. with optimal
+                            networks.
+                        </p>
+                        <div class="card_area">
+                            <div class="product_count_area">
+                                <p>Quantity</p>
+                                <div class="product_count d-inline-block">
+                                    <span class="product_count_item inumber-decrement"> <i class="ti-minus"></i></span>
+                                    <input class="product_count_item input-number" type="text" value="1" min="0" max="10">
+                                    <span class="product_count_item number-increment"> <i class="ti-plus"></i></span>
+                                </div>
+                                <p>$5</p>
+                            </div>
+                            <div class="add_to_cart">
+                                <a href="#" class="btn_3">add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
